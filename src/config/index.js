ab3ee2bf16eb03f8f654927ad29bfdba89ac3ca0
Adeesha Savinda de Silva <adeesha.savinda@gmail.com>
2023-08-16 10:55:00 +0530

    init

--- src/config/index.js
+++ src/config/index.js
@@ -1,8 +1,8 @@
-const IDP_HOST = 'localhost';
-const IDP_PORT = 9443;
+const IDP_HOST = '<HOST> ex: localhost or 192.168.1.100 or identityserver.com';
+const IDP_PORT = '<PORT> ex: 9443 which is the default port for WSO2 Identity Server';
 
 export const IDP_BASE_URL = `https://${IDP_HOST}:${IDP_PORT}`;
 
-export const IDP_DEVICE_AUTH_EP = '/oauth2/device_authorize'; // WSO2 IS device authorize endpoint
+export const IDP_DEVICE_AUTH_EP = `https://${IDP_HOST}:${IDP_PORT}/oauth2/device_authorize`; // WSO2 Identiyty Server default device authorize endpoint
 
-export const CLIENT_ID = 'Jp0PGHmJ424CDVeOScvOcAis1tMa';
+export const CLIENT_ID = '<CLIENT ID OF THE CONFIGURED APPLICATION>';
