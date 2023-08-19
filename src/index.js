d9bc151fd0e46721846925cb48b6ddc7477e5345
Adeesha Savinda de Silva <adeesha.savinda@gmail.com>
2023-08-19 15:40:59 +0530

    store added

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
+
 import './index.css';
 import App from './App';
+import { store } from './store';
+
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-    <App />
+    <Provider store={store}>
+        <App />
+    </Provider>
 );
